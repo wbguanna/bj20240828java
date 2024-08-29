71a7f7521e0d11c6ef75bf5562c7977e929c27d1
jihwan <80106655+Eguanna@users.noreply.github.com>
2024-08-29 14:27:59 +0900

    day24 서블릿 HttpServletResponse html 출력

--- day01/src/main/java/org/comstudy/myweb/App.java
+++ day01/src/main/java/org/comstudy/myweb/App.java
@@ -1,6 +1,8 @@
 package org.comstudy.myweb;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +29,24 @@ public class App extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		
+		// HttpServletResponse의 PrintWriter 객체를 이용해서 화면에 HTML 출력;
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+//		out.println("!<DOCTYPE html>"
+//				+"<head>"
+//				+"<meta charset=\"UTF-8\""
+//				+"<title>App 서블릿 사용법 예제</title>"
+//				+"</head><body>");
+		
+		out.write("<h1>this App run on Tomcat 9.0</h1>\n");
+		out.println("<p>this function printed by PrintWriter.println</p>");
+		
+		
+		out.println("</body></html>");
+		out.close();
 	}
 
 	/**
--- day01/src/main/java/org/comstudy/myweb/model/TodoVo.java
+++ day01/src/main/java/org/comstudy/myweb/model/TodoVo.java
@@ -35,7 +35,7 @@ public class TodoVo {
 		this.title = title;
 	}
 
-	public boolean isDone() {
+	public boolean isDone() {ㅎ
 		return done;
 	}
 
